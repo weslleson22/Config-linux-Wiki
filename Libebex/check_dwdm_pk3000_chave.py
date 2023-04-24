ca125e197d9fa42f623400725e951c6689ff5d92
Weslleson22 <Wesllesonsowza@gmail.com>
2023-04-24 12:23:30 -0300

    Atualização dos plugin pk e add arq .cfg de pirir

--- Libebex/check_dwdm_pk3000_chave.py
+++ Libebex/check_dwdm_pk3000_chave.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 from ast import arguments
 from cmath import log
 import sys
@@ -27,58 +22,10 @@ ip6 = '10.1.60.50' # Imperatriz
 ip7 = '10.1.60.2' # Santa InÊs
 ip8 = '10.1.55.154' #piriripi
 
-if(host == ip8):
-    url = 'http://'+host+'/static/php/mycard/login.php'
-    body = {
-        "id": "0",
-        "name": "0",
-        "data": {
-        "account": "admin",
-        "password": "admin",
-        "captcha": ""
-        }
-    }
-    #print("ok")
-    response = requests.get('http://'+host+'/static/php/mycard/findAll.php')
-    response.raise_for_status()
-    jsonResponse = response.json()
-    #print(jsonResponse)
-    if (mode == 0):
-    ## Pegar o sinal da TX da Placa
-        current=jsonResponse["data"]["cardData"][placa-1]["da"][10]
-    if(mode == 1):
-        ## Pegar o sinal da R1 da Placa
-        current=jsonResponse["data"]["cardData"][placa-1]["da"][8]
-    if(mode == 2):
-        ## Pegar o sinal da R2 da Placa
-        current=jsonResponse["data"]["cardData"][placa-1]["da"][9]
 
 
-    result=float(current)
-
-    rx = result
-
-    diffRx = abs(rx-rxRef)
-
-
-    if(diffRx >= 0 and diffRx <= 1):
-        #print("OK ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
-        print("OK ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
-        exit(OK)
-
-    if(diffRx >1 and diffRx < 3):
-        #print("WARNING ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
-        print("WARNING ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
-        exit(WARNING)
-
-    if(diffRx >= 3):
-        #print("CRITICAL ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
-        print("CRITICAL ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
-        exit(CRITICAL)
-
-elif(host == ip3 or host == ip4 or host == ip6 or host == ip7):
-  url = 'http://'+host+':8081/fast/php/mcSysLogin/login.php'
-  body = {
+url = 'http://'+host+':8081/fast/php/mcSysLogin/login.php'
+body = {
     "id": "0",
     "name": "0",
     "data": {
@@ -87,40 +34,40 @@ elif(host == ip3 or host == ip4 or host == ip6 or host == ip7):
       "captcha": ""
     }
   }
-  response = requests.get('http://'+host+':8081/fast/php/mcCardMonitor/findMonitor.php')
-  response.raise_for_status()
-  jsonResponse = response.json()
+response = requests.get('http://'+host+':8081/fast/php/mcCardMonitor/findMonitor.php')
+response.raise_for_status()
+jsonResponse = response.json()
 
   
-  if (mode == 0):
+if (mode == 0):
     ## Pegar o sinal da TX da Placa
     current=jsonResponse["data"]["cardData"][placa-1]["da"][10]
-  if(mode == 1):
+if(mode == 1):
     ## Pegar o sinal da R1 da Placa
     current=jsonResponse["data"]["cardData"][placa-1]["da"][8]
-  if(mode == 2):
+if(mode == 2):
     ## Pegar o sinal da R2 da Placa
     current=jsonResponse["data"]["cardData"][placa-1]["da"][9]
 
 
-  result=float(current)
+result=float(current)
 
-  rx = result
+rx = result
 
-  diffRx = abs(rx-rxRef)
+diffRx = abs(rx-rxRef)
 
 
-  if(diffRx >= 0 and diffRx <= 1):
+if(diffRx >= 0 and diffRx <= 1):
       #print("OK ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
       print("OK ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
       exit(OK)
 
-  if(diffRx >1 and diffRx < 3):
+if(diffRx >1 and diffRx < 3):
       #print("WARNING ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
        print("WARNING ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
        exit(WARNING)
 
-  if(diffRx >= 3):
+if(diffRx >= 3):
       #print("CRITICAL ",result['rx'],"|'Rx'=",result['rx'],";-26;-40;-15,-25",sep="")
        print("CRITICAL ",result,"|'Tx'=",result,";-26;-40;-15,-25",sep="")
        exit(CRITICAL)
